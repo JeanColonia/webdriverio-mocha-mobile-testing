f3c545469fe0ab0c282004ea74ecbb2360d5eaa5
Jean Colonia <engshka123@gmail.com>
2024-03-02 22:39:03 -0500

    changing conf environments (shared, android and ios)

---
+++ config/wdio.android.conf.js
@@ -0,0 +1,21 @@
+const {config} = require('./wdio.shared.conf');
+const path  = require('path');
+
+config.specs = [
+ '../test/specs/android/*.spec.js',
+]
+
+config.port = 4723;
+config.capabilities = [
+{
+ 'appium:platformName': 'Android',
+ 'appium:platformVersion': '11.0',
+ 'appium:deviceName': 'Pixel 3',
+ 'appium:automationName': 'UIAutomator2',
+ 'appium:app':path.join(process.cwd(),'app/android/ApiDemos-debug.apk'),
+ 'appium:autoGrantPermissions': true
+}
+]
+
+
+exports.config = config;
---
+++ config/wdio.ios.conf.js
@@ -0,0 +1,22 @@
+const {config} = require('./wdio.shared.conf');
+const path  = require('path');
+
+config.specs = [
+ '../test/specs/ios/*.spec.js',
+ 
+];
+
+config.port = 4723;
+
+config.capabilities = [
+{
+ 'appium:platformName': 'ios',
+ 'appium:platformVersion': '14.5',
+ 'appium:deviceName': 'iPhone 12',
+ 'appium:automationName': 'XCUITest',
+ 'appium:app':path.join(process.cwd(),'app/ios/MVCTodo.app')
+}
+];
+
+
+exports.config = config;
